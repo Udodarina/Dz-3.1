cfc7e1967cdb836dc013f3cd5daa31caade10580
Arina Udod <audod6150@gmail.com>
2024-10-25 14:18:15 +0300

    First commit

---
+++ src/BmiService.java
@@ -0,0 +1,17 @@
+import java.text.DecimalFormat;
+public class BmiService {
+    public int calculate(int weight, double height) {
+
+        int resoult;
+
+        if ((weight > 0) || (height>0)) {
+            resoult = (int) (weight / (height * height));
+
+        } else {
+
+            resoult=0;
+        }
+        return resoult;
+    }
+}
+
