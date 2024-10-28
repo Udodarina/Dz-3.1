8dea3d0b4f4148777c970125de4a09fc763af52f
Arina Udod <audod6150@gmail.com>
2024-10-28 17:32:27 +0300

    Second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,15 +1,16 @@
 import java.text.DecimalFormat;
+
 public class BmiService {
     public int calculate(int weight, double height) {
 
         int resoult;
 
-        if ((weight > 0) || (height>0)) {
-            resoult = (int) (weight / (height * height));
+        if ((weight > 0) || (height > 0)) {
+           resoult = (int) (weight / (height * height));
 
         } else {
 
-            resoult=0;
+            resoult = 0;
         }
         return resoult;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         BmiService service = new BmiService();
-        int weight=98;
-        double height=1.87;
+        int weight = 98;
+        double height = 1.87;
 
         int bmi = service.calculate(weight, height);
         System.out.println("Ваш индекс массы тела: " + bmi);
